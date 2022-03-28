4aec882ec329b78403b7224208c28331da793a7d
AnnaClaire Marley <AnnaClaire.Marley@erg.com>
2022-03-28 15:33:48 -0700

    added zone_intersect function

--- py/util.py
+++ py/util.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import geopandas as gpd
 
 # 
 def calc_avs(fp: str) -> pd.DataFrame:
@@ -40,3 +41,20 @@ def calc_avs(fp: str) -> pd.DataFrame:
     # Create the average dataframe of each climate index
     sw_av = sw_clean.groupby("wyear_group").agg("mean")
     return sw_av
+
+
+def zone_intersect(ecoregions: gpd.GeoDataFrame, intersect_layer: gpd.GeoDataFrame, intersect_name: str) -> pd.DataFrame:
+    """Calculate the area of each ecoregion in a hazard or land type
+
+    Args:
+        ecoregions (gpd.GeoDataFrame): shapefile of the ecoregions
+        intersect_layer (gpd.GeoDataFrame): shapefile of the hazard or land type you want to calculate area for
+        intersect_name (str): The name of the hazard
+
+    Returns:
+        pd.DataFrame: a dataframe with the ecoregion in one column and the area in acres of the hazard type in the region
+    """
+    zones = gpd.overlay(ecoregions, intersect_layer, how='intersection')
+    zones[intersect_name] = zones['geometry'].area / 43560
+    zones = zones.drop('geometry', axis = 1)
+    return zones
